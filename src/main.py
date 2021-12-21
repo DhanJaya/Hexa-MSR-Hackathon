6fa45e49c9cb00ae23235a3dd32646c8f59ea052
Chathura Hewa Algiriyage <cpriyankara@ecs.vuw.ac.nz>
2021-12-21 15:12:35 +1300

    Added the py script to retrieve data from the PRs

--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
-
 import repos.repositories as repositories
 import githubapi.apiconnection as apiconnection
+
 
 def main():
    # repositories.download_repo()
     apiconnection.get_open_source_repos()
+
 
 if __name__ == "__main__":
     main()
---
+++ src/pr_data_retrieve.py
@@ -0,0 +1,28 @@
+import argparse
+import json
+import datetime
+import pytz
+from perceval.backends.core.github import GitHub
+
+tz_nz = pytz.timezone('Pacific/Auckland')
+
+parser = argparse.ArgumentParser(
+    description="Simple parser for GitHub issues and pull requests"
+)
+parser.add_argument("-t", "--token", "--nargs", nargs='+', help="GitHub token")
+parser.add_argument("-r", "--repo", help="GitHub repository, as 'owner/repo'")
+
+args = parser.parse_args()
+
+(arg1, arg2, arg3, owner, repo) = args.repo.split('/')
+
+repo = GitHub(owner=owner, repository=repo, api_token=args.token)
+repo.client = repo._init_client()
+
+f_date = tz_nz.localize(datetime.datetime(2020, 12, 10))
+t_date = tz_nz.localize(datetime.datetime(2021, 12, 25))
+
+for item in repo.fetch_items('pull_request', from_date=f_date, to_date=t_date):
+    print(item)
+    with open('pr-data.json', 'a') as j:
+        json.dump(item, j)
