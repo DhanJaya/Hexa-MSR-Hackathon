30ed4b3019c46a1f751f50a898d9db0f6be9f60f
Chathura Hewa Algiriyage <cpriyankara@ecs.vuw.ac.nz>
2021-12-26 01:41:21 +1300

    Added the graal script

--- src/graal-test.py
+++ src/graal-test.py
@@ -4,14 +4,18 @@ import shutil
 
 from graal.backends.core.covuln import CoVuln
 
-repo_uri = "http://github.com/chaoss/grimoirelab-perceval"
+repo_uri = "https://github.com/chaoss/grimoirelab-perceval"
 directory = "/tmp/grimoirelab-perceval"
 
+# The repository should be mirrored to a new directory.
+# If the repository already exists in the given path it won't be mirrored.
+# As a result of the above the worktree won't be crated.
 if os.path.exists(directory):
     shutil.rmtree(directory)
 
 repo_dir = "/tmp/grimoirelab-perceval"
 
+# Entry point should be one of the modules in the project that we need to analyse, for example src/main
 co_vuln = CoVuln(uri=repo_uri, entrypoint="perceval", git_path=repo_dir)
 
 items = co_vuln.fetch()
