6630a8bbdb402c68601348d4fb26c5ca95c3ea46
Chathura Hewa Algiriyage <cpriyankara@ecs.vuw.ac.nz>
2021-12-27 01:18:40 +1300

    Added the graal script

--- src/graal-test.py
+++ src/graal-test.py
@@ -8,13 +8,27 @@ owner = 'chaoss'
 repo = 'grimoirelab-graal'
 directory = '/tmp/' + repo
 
-if os.path.exists(directory):
-    shutil.rmtree(directory)
+
+def remove_directories():
+    if os.path.exists(directory):
+        shutil.rmtree(directory)
+        print('Removed: '+directory)
+    if os.path.exists('/tmp/worktrees'):
+        shutil.rmtree('/tmp/worktrees')
+        print('Removed: /tmp/worktrees')
+
+
+# Remove directories in case they already exist
+remove_directories()
 
 repo_uri = 'https://github.com/' + owner + '/' + repo
-co_vuln = CoVuln(uri=repo_uri, entrypoint='/tmp/worktrees/'+repo, git_path=directory)
+co_vuln = CoVuln(uri=repo_uri, entrypoint='/tmp/worktrees/' + repo, git_path=directory)
 items = co_vuln.fetch()
 for commit in items:
     print(commit['data']['analysis'])
     with open('vulns.json', 'a') as j:
         json.dump(commit, j)
+
+remove_directories()
+
+
