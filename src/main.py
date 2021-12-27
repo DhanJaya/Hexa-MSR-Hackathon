8d6297dc646beff44a584277be380ae024853b54
dhanjaya <dhanujaya2019@gmail.com>
2021-12-27 19:44:25 +0530

    Added code to query the pull request details from a repository.

--- src/main.py
+++ src/main.py
@@ -3,8 +3,10 @@ import githubapi.apiconnection as apiconnection
 
 
 def main():
+
    # repositories.download_repo()
-    apiconnection.get_open_source_repos()
+    #apiconnection.get_open_source_repos()
+    apiconnection.retrieve_pull_requests_with_details('https://github.com/stanfordnlp/stanza')
 
 
 if __name__ == "__main__":
