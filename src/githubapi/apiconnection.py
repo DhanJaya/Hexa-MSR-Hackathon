d3add0c168c33da6ae8245b3e2a159058a6350fb
dhanjaya <dhanujaya2019@gmail.com>
2021-12-31 14:50:51 +0530

    included code to get Pull request data and extract graal json values

--- src/githubapi/apiconnection.py
+++ src/githubapi/apiconnection.py
@@ -2,6 +2,7 @@ import requests
 import os.path
 import time
 from datetime import datetime
+import csv
 
 
 def get_git_auth_token():
@@ -12,7 +13,7 @@ def get_git_auth_token():
 def graphql_query_template(query):
     url = 'https://api.github.com/graphql'
     json = {'query': query}
-    api_token = get_git_auth_token()
+    api_token = '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'#get_git_auth_token()
     headers = {'Authorization': 'token %s' % api_token}
     no_of_tries = 0
     try:
@@ -35,29 +36,34 @@ def graphql_query_template(query):
             print("Exception occurred retry in 5 seconds {}".format(exception))
             time.sleep(5)
 
-
+#stars:>=10  created:2001-01-01..2021-01-01 is:public
 def get_open_source_repos():
-    query = '{ search(query: "is:public stars:>=100 created:2011-01-01..2021-01-01 languages:Python", type: REPOSITORY, first: 100) ' \
-            '{ nodes { ... on Repository { isFork url pullRequests { totalCount } } } pageInfo { ' \
+    query = '{ search(query: "is:public created:2011-01-01..2012-01-01 language:java stars:>=10", type: REPOSITORY, first: 100) ' \
+            '{repositoryCount nodes { ... on Repository { isFork url stargazerCount pullRequests { totalCount } } } pageInfo { ' \
             'hasNextPage endCursor startCursor } } }'
     repo_urls = []
     extract_open_source_repo_urls(repo_urls, query)
+    print("Count " + str(len(repo_urls)))
 
 
 def extract_open_source_repo_urls(repo_urls, query):
     response = graphql_query_template(query)
     if len(response) > 0:
         search_result = response['data']['search']
+        print('Repository count ' + str(search_result['repositoryCount']))
         if len(search_result['nodes']) > 0:
             for repo in search_result['nodes']:
-                if not repo['isFork'] and repo['pullRequests']['totalCount'] >= 10:
-                    repo_urls.append(repo['url'])
-                    print(repo['url'])
+               # repo_urls.append(repo['url'])
+                #print(repo['url'])
+                #repo_urls.append(repo['url'])
+                if not repo['isFork'] and repo['pullRequests']['totalCount'] >= 10 and repo['stargazerCount'] >=10:
+                     repo_urls.append(repo['url'])
+                     print(repo['url'])
         if search_result['pageInfo']['hasNextPage']:
             end_cursor = search_result['pageInfo']['endCursor']
-            query = '{ search(query: "is:public stars:>=100 created:2011-01-01..2021-01-01 languages:Python", type: REPOSITORY,' \
+            query = '{ search(query: "is:public language:java", type: REPOSITORY,' \
                     ' first: 100, after: "%s") ' \
-                    '{ nodes { ... on Repository { isFork url pullRequests { totalCount } } } pageInfo { ' \
+                    '{ repositoryCount nodes { ... on Repository { isFork url stargazerCount pullRequests { totalCount } } } pageInfo { ' \
                     'hasNextPage endCursor startCursor } } }'% (end_cursor)
             extract_open_source_repo_urls(repo_urls, query)
 
@@ -68,18 +74,35 @@ def retrieve_pull_requests_with_details(repository_url):
     query = 'query { repository(name: "%s", owner: "%s") { pullRequests(first: 100) { ' \
             'pageInfo { hasNextPage endCursor } nodes { number id participants { totalCount } reviewThreads(first: ' \
             '100) { totalCount nodes { isResolved } pageInfo { hasNextPage endCursor } } reviews { totalCount } ' \
-            'state commits { totalCount } createdAt mergedAt merged assignees { totalCount } } } } }'% \
+            'state commits (first: 100) { totalCount nodes { commit { oid committedDate authoredDate} } } createdAt mergedAt merged assignees { totalCount } } } } }'% \
             (repo_owner['repo'], repo_owner['owner'])
+    retrieve_pull_request_iteratively(query, repo_owner)
+
+
+def retrieve_pull_request_iteratively(query, repo_owner):
     response = graphql_query_template(query)
     if len(response) > 0:
         search_result = response['data']['repository']['pullRequests']
         if len(search_result['nodes']) > 0:
             for node in search_result['nodes']:
-                pull_request_details[node['number']] = node
-                print(pull_request_details[node['number']])
-
-
-
+                if node['participants']['totalCount'] > 1 and node['reviewThreads']['totalCount'] > 0 and \
+                        node['reviews']['totalCount'] > 1:
+                    for commit_node in node['commits']['nodes']:
+                        print('commit hash ' + commit_node['commit']['oid'] + ' commit date ' +
+                              commit_node['commit']['committedDate'] + ' authored date ' + commit_node['commit']['authoredDate'])
+                    print('pull request ' + str(node['number']))
+                    print('created at ' + node['createdAt'])
+                    if node['merged']:
+                        print('merged at ' + node['mergedAt'])
+                # pull_request_details[node['number']] = node
+        if search_result['pageInfo']['hasNextPage']:
+            end_cursor = search_result['pageInfo']['endCursor']
+            query = 'query { repository(name: "%s", owner: "%s") { pullRequests(first: 100, after: "%s") { ' \
+                    'pageInfo { hasNextPage endCursor } nodes { number id participants { totalCount } reviewThreads(first: ' \
+                    '100) { totalCount nodes { isResolved } pageInfo { hasNextPage endCursor } } reviews { totalCount } ' \
+                    'state commits (first: 100) { totalCount nodes { commit { oid committedDate authoredDate} } } createdAt mergedAt merged assignees { totalCount } } } } }' % \
+                    (repo_owner['repo'], repo_owner['owner'], end_cursor)
+            retrieve_pull_request_iteratively(query, repo_owner)
 
 
 def extract_owner_repo_name_from_url(url):
--- src/graal-cocom-test.py
+++ src/graal-cocom-test.py
@@ -1,11 +1,18 @@
 import json
 import os
 import shutil
+import datetime
+import dateutil.tz
+
+import json
 
 from graal.backends.core.cocom import CoCom
 
-owner = 'chaoss'
-repo = 'grimoirelab-graal'
+#https://github.com/apache/tomcat
+#https://github.com/alibaba/fastjson
+#https://github.com/apache/ambari
+owner = 'apache'
+repo = 'tomcat'
 directory = '/tmp/' + repo
 
 
@@ -20,15 +27,32 @@ def remove_directories():
 
 # Remove directories in case they already exist
 remove_directories()
+#2017-01-18T07:01:56Z
+from_date = datetime.datetime(2017, 1, 17, 00, 00, 00,
+                              tzinfo=dateutil.tz.tzutc())
+#2017-04-28T18:20:56Z
+to_date = datetime.datetime(2017, 4, 29, 00, 00, 00,
+                                     tzinfo=dateutil.tz.tzutc())
 
 repo_uri = 'https://github.com/' + owner + '/' + repo
 cocom = CoCom(uri=repo_uri, git_path=directory)
 items = cocom.fetch()
+
+
+file1 = open("myfile1.txt","a")
+
+
 for commit in items:
-    print(commit)
+    print(commit['data']['commit'] + ", ")
+    print(commit['data']['AuthorDate'] + ", ")
+    print(commit['data']['analysis'] )
+    file1.write(commit['data']['commit'] + ", ")
+    file1.write(commit['data']['AuthorDate'] + ", ")
+    file1.write(json.dumps(commit['data']['analysis']))
+    file1.write('\n')
     # with open('vulns.json', 'a') as j:
     #     json.dump(commit, j)
-
+file1.close()
 remove_directories()
 
 
