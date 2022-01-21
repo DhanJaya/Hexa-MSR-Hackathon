9c2e5056e1f408e4f76d54f60bf6887b80f3038e
dhanjaya <dhanujaya2019@gmail.com>
2022-01-21 08:19:13 +0530

    Added pr created date and start commit to consider for analysis

--- src/main.py
+++ src/main.py
@@ -6,10 +6,10 @@ import githubapi.apiconnection as apiconnection
 import analysis.codecomplexityanalysis as cocom_analysis
 
 
-def main():
+def retreive_pr_details():
     #repo_urls = apiconnection.get_open_source_repos()
     #repo_urls = ['https://github.com/spring-projects/spring-boot', 'https://github.com/elastic/elasticsearch', 'https://github.com/ReactiveX/RxJava', 'https://github.com/google/guava', 'https://github.com/apache/tomcat', 'https://github.com/dbeaver/dbeaver', 'https://github.com/greenrobot/EventBus', 'https://github.com/SeleniumHQ/selenium', 'https://github.com/google/gson', 'https://github.com/jenkinsci/jenkins', 'https://github.com/redisson/redisson', 'https://github.com/apache/flink', 'https://github.com/mybatis/mybatis-3', 'https://github.com/oracle/graal']
-    repo_urls = ['https://github.com/pallets/flask']
+    repo_urls = ['https://github.com/ReactiveX/RxJava']
 
     #df = pd.read_csv('/mnt/d/hackathon/elasticsearch.csv')
     #already_processed = df['PullNo'].unique().tolist()
@@ -24,28 +24,49 @@ def main():
                 participants = detail['participants']
                 head_repository = detail['headRepository']
                 commits = list(detail['commits'].keys())
-                add_pr_details_to_csv(url, number, head_repository, start_date, end_date, commits, participants)
+                pr_created = detail['prCreated']
+                commit_before_pr = detail['commitBeforePR']
 
-              #  print('pull request' + str(number))
-               # commit_analysis = cocom_analysis.run_graal_analysis(head_repository, start_date, end_date, detail['commits'].keys())
-               # if commit_analysis is not None and len(commit_analysis) > 0:
-               #     for commit, analysis in commit_analysis.items():
-               #         append_to_file(url, number, participants, commit,  analysis)
+                add_pr_details_to_csv(url, number, head_repository, start_date, end_date, commits, pr_created, commit_before_pr, participants)
 
 
-def add_pr_details_to_csv(repo, pr_no, fork, start_date, end_date, commits, participants):
-    file_exist = os.path.exists('/mnt/d/hackathon/prdata.csv')
-    with open('/mnt/d/hackathon/prdata.csv', 'a', newline='') as csv_file:
+def run_graal_analysis():
+    already_processed = []
+    if os.path.exists('/mnt/d/hackathon/httpie.csv'):
+        df = pd.read_csv('/mnt/d/hackathon/httpie.csv')
+        already_processed.extend(df['PullNo'].unique().tolist())
+    with open('/mnt/d/hackathon/httpieprdata.csv', encoding="utf8") as f:
+        csv_reader = csv.DictReader(f)
+        # skip the header
+        next(csv_reader)
+        # show the data
+        for line in csv_reader:
+            if int(line['PullRequest']) not in already_processed:
+                print(f"Processing PR {line['PullRequest']} in  {line['Fork']} ")
+                commit_analysis = cocom_analysis.run_graal_analysis(line['Fork'], line['Start_date'], line['End_date'],
+                                                                    line['Commits'])
+                if commit_analysis is not None and len(commit_analysis) > 0:
+                    for commit, analysis in commit_analysis.items():
+                         append_to_file(line['Repo'], line['PullRequest'], line['Participants'], commit,  analysis)
+
+
+def add_pr_details_to_csv(repo, pr_no, fork, start_date, end_date, commits, pr_created, commit_before_pr, participants):
+    file_exist = os.path.exists('/mnt/d/hackathon/reactRXprdata1.csv')
+    with open('/mnt/d/hackathon/reactRXprdata1.csv', 'a', newline='') as csv_file:
         writer = csv.writer(csv_file)
         if not file_exist:
-            writer.writerow(['Repo', 'PullRequest', 'Fork', 'Start_date', 'End_date', 'Commits', 'Participants'])
-        writer.writerow([repo, pr_no, fork, start_date, end_date, commits, participants])
+            writer.writerow(['Repo', 'PullRequest', 'Fork', 'Start_date', 'End_date', 'Commits', 'PR_Start', 'Commit_Before_PR', 'Participants'])
+        writer.writerow([repo, pr_no, fork, start_date, end_date, commits, pr_created, commit_before_pr, participants])
 
 def append_to_file(url, pr_number, participants, commit_hash, analysis):
-    with open('/mnt/d/hackathon/elasticsearch.csv', 'a', newline='') as csv_file:
+    file_exist = os.path.exists('/mnt/d/hackathon/httpie.csv')
+    with open('/mnt/d/hackathon/httpie.csv', 'a', newline='') as csv_file:
         writer = csv.writer(csv_file)
+        if not file_exist:
+            writer.writerow(['URL', 'PullNo', 'Participants', 'CommitHash', 'Analysis'])
         writer.writerow([url, pr_number, participants, commit_hash, analysis])
 
 
 if __name__ == "__main__":
-    main()
+   retreive_pr_details()
+   #run_graal_analysis()
