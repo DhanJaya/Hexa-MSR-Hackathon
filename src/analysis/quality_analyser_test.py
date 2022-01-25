0697eb0b52061135a33a61b5d984f00027c48d98
Chathura Priyankara <priyankarahac@gmail.com>
2022-01-26 00:00:49 +1300

    Added the code quality test code

---
+++ src/analysis/quality_analyser_test.py
@@ -0,0 +1,36 @@
+import datetime
+import os
+import shutil
+
+from graal.backends.core.coqua import CoQua, CATEGORY_COQUA_FLAKE8, CATEGORY_COQUA_PYLINT
+
+
+def remove_directories(directory):
+    if os.path.exists(directory):
+        shutil.rmtree(directory)
+        print('Removed: ' + directory)
+    if os.path.exists('/tmp/worktrees'):
+        shutil.rmtree('/tmp/worktrees')
+        print('Removed: /tmp/worktrees')
+
+
+def run_quality_analysis():
+    repo = 'flask'
+    directory = '/tmp/' + repo
+    repo_uri = 'https://github.com/r0fls/flask'
+
+    remove_directories(directory)
+
+    start_date_time = datetime.datetime.strptime('2017-01-16T00:00:00Z', '%Y-%m-%dT%H:%M:%SZ') - datetime.timedelta(2)
+    end_date_time = datetime.datetime.strptime('2017-01-17T00:00:00Z', '%Y-%m-%dT%H:%M:%SZ') + datetime.timedelta(days=2)
+
+    # Entrypoint should be the source root
+    coqua = CoQua(uri=repo_uri, entrypoint='flask', git_path=directory)
+    items = coqua.fetch(from_date=start_date_time, to_date=end_date_time, category=CATEGORY_COQUA_PYLINT)
+
+    for commit in items:
+        print(commit)
+    remove_directories(directory)
+
+
+run_quality_analysis()
