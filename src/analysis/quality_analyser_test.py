b7b4da6261caf54262375016d8319d39b2cb89d7
Chathura Priyankara <priyankarahac@gmail.com>
2022-01-26 00:28:43 +1300

    Added the code quality test code (this script can be deleted later)

--- src/analysis/quality_analyser_test.py
+++ src/analysis/quality_analyser_test.py
@@ -3,7 +3,6 @@ import os
 import shutil
 
 from graal.backends.core.coqua import CoQua, CATEGORY_COQUA_FLAKE8, CATEGORY_COQUA_PYLINT
-from graal.backends.core.covuln import CoVuln
 
 
 def remove_directories(directory):
@@ -26,8 +25,8 @@ def run_quality_analysis():
     end_date_time = datetime.datetime.strptime('2017-01-17T00:00:00Z', '%Y-%m-%dT%H:%M:%SZ') + datetime.timedelta(days=2)
 
     # Entrypoint should be the source root (Usually source root = repo name)
-    coqua = CoVuln(uri=repo_uri, entrypoint='/tmp/worktrees/flask', git_path=directory)
-    items = coqua.fetch(from_date=start_date_time, to_date=end_date_time)
+    coqua = CoQua(uri=repo_uri, entrypoint='flask', git_path=directory)
+    items = coqua.fetch(from_date=start_date_time, to_date=end_date_time, category=CATEGORY_COQUA_PYLINT)
 
     for commit in items:
         print(commit)
